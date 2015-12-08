97cff112c64e3d982cb203b7d1f5cab66ef85523
Omid Nasri <omid.nasri@outlook.com>
2015-12-08 23:41:15 +0330

    Initialize

---
+++ SessionTimeout/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionTimeout.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// HTTP-GET
+        /// </summary>
+        /// <returns>VIEW</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// HTTP-POST
+        /// </summary>
+        /// <returns>LOGOFF - REDIRECT TO LOGIN PAGE</returns>
+        public ActionResult LogOff()
+        {
+            // Code
+            // ...
+            return RedirectToAction("Login","Home");
+        }
+        /// <summary>
+        /// HTTP-GET
+        /// </summary>
+        /// <returns>VIEW</returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
